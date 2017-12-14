72a2765dcdc9091090ee5b08c42059c73b650b53
phantom <354073823@qq.com>
2017-12-14 23:08:40 +0800

    data

--- src/App.vue
+++ src/App.vue
@@ -11,7 +11,7 @@ export default {
   mounted () {
     //  do something after mounting vue instance
     let url = 'http://localhost/api/Article'
-    this.$http.get(url + '?title=0').then(response => {
+    this.$http.get(url + '?title=1').then(response => {
       console.log(response)
     })
   }
