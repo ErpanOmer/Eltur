043f3ee417c55b999a60321126423b5b889c2217
phantom <354073823@qq.com>
2017-12-15 11:01:30 +0800

    add admin project

---
+++ front/src/App.vue
@@ -0,0 +1,29 @@
+<template>
+  <div id="app">
+    <img src="./assets/logo.png">
+    <router-view/>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'app',
+  mounted () {
+    //  do something after mounting vue instance
+    let url = 'http://localhost/api/Article'
+    this.$http.get(url + '?title=2').then(response => {
+      console.log(response)
+    })
+  }
+}
+</script>
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
--- server/index.js
+++ server/index.js
@@ -24,10 +24,18 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(Article);
 // 访问静态资源文件 这里是访问所有dist目录下的静态资源文件
-app.use(express.static(path.resolve(__dirname, '../dist')))
+//   eltur  前端模板
+app.use(express.static(path.resolve(__dirname, '../front/dist')))
+//    eltur  后台模板
+app.use(express.static(path.resolve(__dirname, '../admin/dist')))
 // 因为是单页应用 所有请求都走/dist/index.html
-app.get('*', function(req, res) {
-  const html = fs.readFileSync(path.resolve(__dirname, '../dist/index.html'), 'utf-8')
+app.get('/', function(req, res) {
+  const html = fs.readFileSync(path.resolve(__dirname, '../front/dist/index.html'), 'utf-8')
+  res.send(html)
+})
+//   如果是后台  进入这个
+app.get('/admin', function(req, res) {
+  const html = fs.readFileSync(path.resolve(__dirname, '../admin/dist/index.html'), 'utf-8')
   res.send(html)
 })
 // 监听7373端口
