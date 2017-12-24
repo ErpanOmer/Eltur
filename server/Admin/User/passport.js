03161a8e64dbb3e569e4648a2ead4260c03eb26b
phantom <354073823@qq.com>
2017-12-25 00:24:58 +0800

    token

---
+++ server/Admin/User/passport.js
@@ -0,0 +1,23 @@
+const passport = require('passport');
+const Strategy = require('passport-http-bearer').Strategy;
+
+const User = require('./User.db.js');
+const config = require('../../db.config.js')
+
+module.exports = function(passport) {
+    passport.use(new Strategy(
+        function(token, done) {
+            User.findOne({
+                token: token
+            }, function(err, user) {
+                if (err) {
+                    return done(err);
+                }
+                if (!user) {
+                    return done(null, false);
+                }
+                return done(null, user);
+            });
+        }
+    ));
+};
--- server/index.js
+++ server/index.js
@@ -1,15 +1,18 @@
+// 引入Express
+const express = require('express');
 // 引入编写好的api
 const Article = require('./Application/Article/Article.api')
+const User = require('./Admin/User/User.api')
 // 引入文件模块
 const fs = require('fs');
 // 引入处理路径的模块
 const path = require('path');
 // 引入处理post数据的模块
 const bodyParser = require('body-parser')
-// 引入Express
-const express = require('express');
 //   日志插件
 const morgan = require('morgan')
+const passport = require('passport');// 用户认证模块passport
+const Strategy = require('passport-http-bearer').Strategy;// token验证模块
 const app = express();
 // 自定义token
 morgan.token('from', function(req, res){
@@ -19,6 +22,11 @@ morgan.token('from', function(req, res){
 morgan.format('Phantom', '[Phantom] :method :url :status :from');
 // 使用自定义的format
 app.use(morgan('Phantom'));
+app.use(passport.initialize());// 初始化passport模块
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(Article);
+app.use(User);
 //   配合前端 history 模式
 app.all('*',function (req, res, next) {
   res.header('Access-Control-Allow-Origin', '*');
@@ -31,9 +39,6 @@ app.all('*',function (req, res, next) {
     next();
   }
 });
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({extended: false}));
-app.use(Article);
 // 访问静态资源文件 这里是访问所有dist目录下的静态资源文件
 // 因为是单页应用 所有请求都走/dist/index.html
 app.get(/index/, function(req, res) {
