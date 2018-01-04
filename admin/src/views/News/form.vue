7c8aa44dad410fade3c214231a6897682646912b
phantom <354073823@qq.com>
2018-01-04 23:38:33 +0800

    form

--- admin/src/views/News/form.vue
+++ admin/src/views/News/form.vue
@@ -1,16 +1,16 @@
 <template>
   <div class="app-container">
     <el-form ref="form" :model="form" label-width="120px">
-      <el-form-item label="Activity name">
+      <el-form-item label="新闻标题">
         <el-input v-model="form.name"></el-input>
       </el-form-item>
-      <el-form-item label="Activity zone">
+      <!-- <el-form-item label="Activity zone">
         <el-select v-model="form.region" placeholder="please select your zone">
           <el-option label="Zone one" value="shanghai"></el-option>
           <el-option label="Zone two" value="beijing"></el-option>
         </el-select>
-      </el-form-item>
-      <el-form-item label="Activity time">
+      </el-form-item> -->
+      <!-- <el-form-item label="Activity time">
         <el-col :span="11">
           <el-date-picker type="date" placeholder="Pick a date" v-model="form.date1" style="width: 100%;"></el-date-picker>
         </el-col>
@@ -18,10 +18,10 @@
         <el-col :span="11">
           <el-time-picker type="fixed-time" placeholder="Pick a time" v-model="form.date2" style="width: 100%;"></el-time-picker>
         </el-col>
-      </el-form-item>
-      <el-form-item label="Instant delivery">
+      </el-form-item> -->
+      <!-- <el-form-item label="Instant delivery">
         <el-switch v-model="form.delivery"></el-switch>
-      </el-form-item>
+      </el-form-item> -->
       <el-form-item label="Activity type">
         <el-checkbox-group v-model="form.type">
           <el-checkbox label="Online activities" name="type"></el-checkbox>
@@ -82,4 +82,3 @@ export default {
   text-align: center;
 }
 </style>
-
