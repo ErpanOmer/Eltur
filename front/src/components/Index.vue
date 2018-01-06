b273937537108c463889c3ec10c7fb7c62ccfc7a
ErpanOmer <354073823@qq.com>
2018-01-06 13:47:49 +0800

    test

--- front/src/components/Index.vue
+++ front/src/components/Index.vue
@@ -1,35 +1,18 @@
 <template>
-  <div class="hello">
-    <h1>{{msg}}</h1>
+  <div class="">
+    {{msg}
   </div>
 </template>
-
 <script>
 export default {
-  data () {
+  data: function () {
     return {
-      msg: 'Peherdin'
+      msg: 'ddd'
     }
   },
-  mounted () {
+  mounted() {
   }
 }
 </script>
-
-<!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped>
-h1, h2 {
-  font-weight: normal;
-}
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
-}
+<style lang="css" scoped>
 </style>
