428912fc2bd1ae0ab8f7bc956f6ca9345c03a230
phantom <354073823@qq.com>
2018-04-12 23:57:08 +0800

    issue

--- server/Application/Model/issue.js
+++ server/Application/Model/issue.js
@@ -2,3 +2,46 @@ const mongoose = require('mongoose');
 const config = require('../../db.config.js')
 const Schema = mongoose.Schema;
 const bcrypt = require('bcrypt');
+
+const Issue = new Schema({
+  question: {
+    type: String,
+    default: ''
+  },
+  cover: {
+    type: String,
+    default: ''
+  },
+  name: {
+    type: String,
+    default: ''
+  },
+  category: {
+    type: Number,
+    default: ''
+  },
+  views: {
+    type: Number,
+    default: 0
+  },
+  alreadyAnswered: {
+    type: Boolean,
+    default: false
+  },
+  answerText: {
+    type: String,
+    default: ''
+  },
+  fabulous:{
+    type: Number,
+    default: 0
+  },
+  createdTime: {
+    type: Number,
+    default: 0
+  },
+  updateTime: {
+    type: Number,
+    default: 0
+  }
+})
