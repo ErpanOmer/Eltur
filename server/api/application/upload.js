b01f9d6bef7974fbc11aa8839d29d9af2868cf25
unknown <354073823@qq.com>
2018-04-20 14:52:42 +0800

    api

--- server/api/application/upload.js
+++ server/api/application/upload.js
@@ -3,7 +3,7 @@ const router = express.Router();
 const upload = require('../../common/upload');
 const api = '/application/'
 
-router.use(`${api}upload`, require('./Interceptor.js'))
+router.use(`${api}upload`, require('./interceptor.js'))
 router.post(`${api}upload`, function(req, res, next) {
   upload(req, res, next);
 })
